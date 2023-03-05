c726d301b1c1b1c3efb453385cb78cc69dd65456
AnxianZhang <gadanxianzhang@gmail.com>
2023-03-05 12:13:26 +0100

    Abonne and DVD class added

--- src/abonnee/Abonne.java
+++ src/abonnee/Abonne.java
@@ -1,4 +1,13 @@
 package abonnee;
 
+import document.Document;
+
+import java.util.List;
+
 public class Abonne {
+    private int numAbonee;
+    private String dateNaissance; //car format aaaa-mm-jj
+    private String nom;
+    private List<Document> listReserve;
+    private List<Document> listEmprunte;
 }
---
+++ src/document/DVD.java
@@ -0,0 +1,66 @@
+package document;
+
+import abonnee.Abonne;
+
+public class DVD implements Document{
+    private int numDVD;
+    private boolean estAdulte;
+
+    private int numDoc;
+    private boolean estReserve;
+    private boolean estEmprunte;
+    private boolean estRetourne;
+    private String titre;
+    private int numAbonee;
+    private Abonne abonne;
+
+    public DVD(){
+        this.estReserve=false;
+        this.estEmprunte=false;
+        this.estRetourne=true;
+    }
+
+    @Override
+    public int numero() {
+        return this.numDoc;//j'ai pas sur c quel num
+    }
+
+    @Override
+    public Abonne emprunteur() {
+        if (estEmprunte)
+            return this.abonne;
+        else return null;
+    }
+
+    @Override
+    public Abonne reserveur() {
+        if (estReserve)
+            return this.abonne;
+        else return null;
+    }
+
+    @Override
+    public void reservationPour(Abonne ab) {
+        if (!estReserve & !estEmprunte) {
+            this.abonne=ab;
+            this.estReserve = true;
+            this.estRetourne=false;
+        }
+    }
+
+    @Override
+    public void empruntPar(Abonne ab) {
+        if (this.reserveur()==ab|!estEmprunte) {
+            this.abonne=ab;
+            this.estEmprunte = true;
+            this.estRetourne=false;
+        }
+    }
+
+    @Override
+    public void retour() {
+        this.estReserve=false;
+        this.estEmprunte=false;
+        this.estRetourne=true;
+    }
+}
